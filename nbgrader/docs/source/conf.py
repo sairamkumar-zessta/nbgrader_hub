13f4c1323bd2ea1cbc1aa9b0b55ac3377e6d0cfd
admin <sairamkumar.alladi@zessta.com>
2023-03-30 15:24:02 +0530

    added fav

--- nbgrader/docs/source/conf.py
+++ nbgrader/docs/source/conf.py
@@ -135,7 +135,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = ['https://datalab-static-images.s3.ap-south-1.amazonaws.com/career_shaper_fav_icon.png']
+html_favicon = 'https://datalab-static-images.s3.ap-south-1.amazonaws.com/career_shaper_fav_icon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
